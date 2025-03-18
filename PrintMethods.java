b58cc02f9acc100053a3275ed9b469013517ba66
OluwaFemi Arogundade <oluwafemiarogundade59@gmail.com>
2025-03-18 15:45:56 +0100

    print method

---
+++ PrintMethods.java
@@ -0,0 +1,18 @@
+public class PrintMethods{
+	public static void main(String[] args){
+		//using the print()
+		System.out.print("\"Learning java is Fun\"");
+		System.out.print(" Hey, come and learn java with me");
+		
+		//using the rint()
+		System.out.println();
+		System.out.println("I want to tell you that java is different from javascript");
+		System.out.println("\t this is the indented text");
+		
+		//using the printf()
+		System.out.printf("My name is %s and i love %s%n","Femi","java");
+		System.out.printf("I am living in No. %d Kaduna street%n",1);
+		System.out.printf("I bought java JDK with %c%f %n",'$',159.345);
+		System.out.printf("I bought java JDK with %.2f Naria%n",159.345);
+	}
+}	
