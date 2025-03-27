aaaf528220e14a3ab80a8d944c44cc5860fa0ac2
OluwaFemi Arogundade <oluwafemiarogundade59@gmail.com>
2025-03-27 16:10:07 +0100

    For Loop

---
+++ PrintName.java
@@ -0,0 +1,9 @@
+public class PrintName {
+    public static void main(String[] args) {
+        String name = "Oluwa Femi"; 
+		
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name);
+        }
+    }
+}
